535d8fd34d9915e3a2c22420bd559a1b7ea86212
unknown <csumin008255@gmail.com>
2022-03-25 17:51:51 +0900

    Updtae node.js folder

--- node.js/main.js
+++ node.js/main.js
@@ -5,14 +5,37 @@ var url = require("url");
 var app = http.createServer(function (request, response) {
   var _url = request.url;
   var queryData = url.parse(_url, true).query;
-  console.log(queryData.id);
-  if (_url == "/") {
-    _url = "/index.html";
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
+
+  if (pathname === "/") {
+    fs.readFile(`data/${queryData.id}`, "utf8", function (err, description) {
+      var template = `
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8" />
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ul>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JAVASCRIPT">JavaScript</a></li>
+      </ul>
+      <h2>${title}</h2>
+      <p>${description}</p>
+    </body>
+  </html>
+  
+    `;
+      response.writeHead(200); // 200: 파일을 성공적으로 전송
+      response.end(template);
+    });
+  } else {
+    response.writeHead(404); // 404: 에러발생
+    response.end("Not Found");
   }
-  if (_url == "/favicon.ico") {
-    return response.writeHead(404);
-  }
-  response.writeHead(200);
-  response.end(queryData.id);
 });
 app.listen(3000);
