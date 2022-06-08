aca0060fa238e5de9d25bd3bfeaac6657647b3ae
unknown <csumin008255@gmail.com>
2022-06-09 02:12:39 +0900

    미들웨워의 사용

--- Nodejs-master/main copy.js
+++ Nodejs-master/main copy.js
@@ -1,27 +1,26 @@
 var express = require("express"); //모듈을 불러오기
 var app = express(); //express(함수)의 리턴값을 app에 저장
 var fs = require("fs");
+var template = require("./lib/template.js");
 var path = require("path");
+var sanitizeHtml = require("sanitize-html");
 var qs = require("querystring");
 var bodyParser = require("body-parser");
-var sanitizeHtml = require("sanitize-html");
 var compression = require("compression");
-var template = require("./lib/template.js");
 
 // bodyParser.urlencoded({ extended: false }) : body-parser가 만들어내는 미들웨어를 표현하는 표현식
 // 사용자가 요청할 떄마다 표현식에 의해 만들어진 미들웨어가 실행됨 (미들웨어가 어떻게 생겼는지는 모름)
 // 사용자가 전송한 post 데이터를 내부적으로 분석해서 경로에 해당되는 콜백함수를 호출하기로 약속
 // 호출하면서 콜백의 첫번째 인자(req.body)인 req에 body라는 프로퍼티를 만들어줌
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(compression()); //app.use를 통해서 compression 모듈을 호출. compression 모듈이 미들웨어를 return. app.use를 통해서 미들웨어 장착
-app.get("*", function (request, response, next) {
-  //function은 미들웨어
+app.use(compression());
+app.get("*", function (requset, response, next) {
+  // get 방식의 모든('*') 요청에 filelist 제공(post방식은 사용X)
   fs.readdir("./data", function (error, filelist) {
-    request.list = filelist;
-    next();
+    requset.list = filelist; // 모든 route안에서 request 객체의 list 프로퍼티를 이용해서 글 목록에 접근 가능
+    next(); // 그 다음에 호출되어야할 미들웨어
   });
 });
-// // 요청이 들어올때마다 body-parser, compression 실행
 
 // app.get("/", function (req, res) {
 //   return res.send("Hello world");
@@ -29,11 +28,7 @@ app.get("*", function (request, response, next) {
 
 //get : route, routing : 방향을 잡는
 //localhost:3000/ 으로 접속할 경우
-
-//route, routing
-//app.get('/', (req, res) => res.send('Hello World!'))
-app.get("/", function (request, response) {
-  //function은 미들웨어임
+app.get("/", (request, response) => {
   var title = "Welcome";
   var description = "Hello, Node.js";
   var list = template.list(request.list);
@@ -61,11 +56,11 @@ app.get("/page/:pageId", function (request, response) {
       list,
       `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
       ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
+                    <a href="/update/${sanitizedTitle}">update</a>
+                    <form action="/delete_process" method="post">
+                      <input type="hidden" name="id" value="${sanitizedTitle}">
+                      <input type="submit" value="delete">
+                    </form>`
     );
     response.send(html);
   });
@@ -77,17 +72,16 @@ app.get("/create", function (request, response) {
   var html = template.HTML(
     title,
     list,
-    `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `,
+    `<form action="/create_process" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `,
     ""
   );
   response.send(html);
@@ -111,21 +105,21 @@ app.get("/update/:pageId", function (request, response) {
     var html = template.HTML(
       title,
       list,
-      `
-        <form action="/update_process" method="post">
+      `<form action="/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
             <textarea name="description" placeholder="description">${description}</textarea>
           </p>
-          <p>
+           <p>
             <input type="submit">
-          </p>
+           </p>
         </form>
         `,
       `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
     );
-    response.send(html);
+    response.writeHead(200);
+    response.end(html);
   });
 });
 
@@ -136,12 +130,14 @@ app.post("/update_process", function (request, response) {
   var description = post.description;
   fs.rename(`data/${id}`, `data/${title}`, function (error) {
     fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      response.redirect(`/?id=${title}`);
+      response.redirect(`/page/${title}`);
     });
   });
 });
 
 app.post("/delete_process", function (request, response) {
+  console.log("-------------------");
+  console.log(request.body);
   var post = request.body;
   var id = post.id;
   var filteredId = path.parse(id).base;
@@ -152,5 +148,34 @@ app.post("/delete_process", function (request, response) {
 
 //listen : 3000번 포트에 리스닝, 성공하면 console.log 실행
 app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+  console.log(`Example app listening on port 3000`);
 });
+
+// var http = require('http');
+// var url = require('url');
+
+// var app = http.createServer(function(request,response){
+//     var _url = request.url;
+//     var queryData = url.parse(_url, true).query;
+//     var pathname = url.parse(_url, true).pathname;
+//     if(pathname === '/'){
+//       if(queryData.id === undefined){
+
+//       } else {
+
+//       }
+//     } else if(pathname === '/create'){
+//
+//     } else if(pathname === '/create_process'){
+//     } else if(pathname === '/update'){
+//
+//     } else if(pathname === '/update_process'){
+//
+//     } else if(pathname === '/delete_process'){
+//
+//     } else {
+//       response.writeHead(404);
+//       response.end('Not found');
+//     }
+// });
+// app.listen(3000);
